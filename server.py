aaa508249b08eb3bbe18f3120483c7d975a28988
Nathan <nathan.leakeas@gmail.com>
2025-04-30 20:57:44 -0400

    simple dns handler

--- server.py
+++ server.py
@@ -8,18 +8,23 @@ from dnslib import DNSRecord
 waiting_for_query = []
 domain = "cs528proj.com"
 
-#def handle_dns_query():
-#    while True:
+def handle_dns_query(dns_listener: socket.socket):
+    while True:
+        data, src = dns_listener.recvfrom(1024)
+        dns_req = DNSRecord.parse(data)
+        qname = dns_req.q.qname
 
 
 def main():
-    #dns_listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    dns_listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     control_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    #dns_listener.bind(('localhost', 53))
+    dns_listener.bind(('localhost', 53))
     control_sock.bind(('localhost', 55555))
     control_sock.listen(5)
     print("Bound sockets")
+    dns_thread = threading.Thread(target=handle_dns_query, args=(dns_listener))
+    dns_thread.start()
     while True:
         client_sock, client_addr = control_sock.accept()
         rand_prefix = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
